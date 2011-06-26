5ac91a4093e7da5e24f7b64d8c95e015630431bb
Agana <nsiire@gmail.com>
2011-06-26 09:22:44 +0000

    fin commit

--- gaeinstaller/installgae.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-tar xzvf gae.tar.gz
-sudo mv google_appengine /usr/local/
-
-#install python
-sudo add-apt-repository ppa:fkrull/deadsnakes
-sudo apt-get update
-sudo apt-get install python2.5
-
